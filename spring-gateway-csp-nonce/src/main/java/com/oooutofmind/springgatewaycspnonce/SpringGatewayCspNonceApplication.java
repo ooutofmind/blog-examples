3fa3b41077fd844248991167cc267684f6fde530
Vladimir Parfenov1 <vladimir_parfenov1@epam.com>
2023-03-28 00:18:51 +0400

    spring-gateway-csp-nonce project init

---
+++ spring-gateway-csp-nonce/src/main/java/com/oooutofmind/springgatewaycspnonce/SpringGatewayCspNonceApplication.java
@@ -0,0 +1,13 @@
+package com.oooutofmind.springgatewaycspnonce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGatewayCspNonceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGatewayCspNonceApplication.class, args);
+	}
+
+}
---
+++ spring-gateway-csp-nonce/src/test/java/com/oooutofmind/springgatewaycspnonce/SpringGatewayCspNonceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oooutofmind.springgatewaycspnonce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGatewayCspNonceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
